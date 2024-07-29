a8063ecf73d0d7f1b5dadb5db5c51b779092ebed
Krisna Restu Dewa <bartelsi.studio@gmail.com>
2024-07-29 18:58:09 +0700

    day 8

---
+++ 8-arrayPlusArray.js
@@ -0,0 +1,12 @@
+function arrayPlusArray(arr1, arr2) {
+    let total = 0;
+    for( let i = 0; i<arr1.length; i++) {
+       total += arr1[i];
+    }
+    for( let i = 0; i<arr2.length; i++) {
+        total += arr2[i];
+     }
+    return total;
+  }
+
+  console.log(arrayPlusArray([1,2,3], [4,5,6]))
